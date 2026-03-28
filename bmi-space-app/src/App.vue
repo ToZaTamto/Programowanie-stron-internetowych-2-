b39385659d81dccb24ec68617fbc6550e88a45bb
ToZaTamto <129876774+ToZaTamto@users.noreply.github.com>
2026-03-28 15:34:13 +0100

    Add files via upload

---
+++ bmi-space-app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import BmiCalculator from './components/BmiCalculator.vue'
+</script>
+
+<template>
+  <main class="app-shell">
+    <div class="stars stars-a"></div>
+    <div class="stars stars-b"></div>
+    <div class="nebula nebula-left"></div>
+    <div class="nebula nebula-right"></div>
+
+    <BmiCalculator />
+  </main>
+</template>
