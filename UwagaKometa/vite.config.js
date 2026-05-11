cf7723b0493a45c8446527840f88c0a38d760990
ToZaTamto <ToZaTamto@users.noreply.github.com>
2026-05-11 22:09:55 +0200

    Publish UwagaKometa from Pages root

--- UwagaKometa/vite.config.js
+++ UwagaKometa/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Programowanie-stron-internetowych-2-/UwagaKometa/',
+  base: './',
   server: {
     port: 5173,
     open: true
